7a81c880db3688cc3f93400da3eec9a8bf50e7b4
xJustJqy <78117567+xJustJqy@users.noreply.github.com>
2021-03-05 00:53:38 -0800

    Started Configuration process

--- src/xjustjqy/MultiServer/Loader.php
+++ src/xjustjqy/MultiServer/Loader.php
@@ -3,18 +3,31 @@
 namespace xjustjqy\MultiServer;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 
 class Loader extends PluginBase {
 
   /** @var self */
-  private $instance;
+  private static $instance;
+  /** @var string */
+  private static $dataFolder;
 
   public function onEnable() {
     self::$instance = $this;
+    self::$dataFolder = str_replace("plugins", "", $this->getFile());
+    self::$settings_manager = new SettingsManager();
   }
 
-  public static function getInstance() : self {
+  public static function getInstance() : ?self {
     return self::$instance;
+  }
+
+  public static function getConfigFolder() : ?string {
+    return self::dataFolder;
+  }
+
+  public static function getSettings() : ?Config {
+    return self::$settings_manager->fetchConfig();
   }
 
 }
